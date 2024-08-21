309fb02d43897c556e77ddce1b970c7bc64f23c4
suey <ho055@csiro.au>
2024-08-21 13:05:33 +1000

    include router and primevue in main.js

--- FIT5032-Assignment/src/main.js
+++ FIT5032-Assignment/src/main.js
@@ -1,6 +1,17 @@
-import './assets/main.css'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import router from './router'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+app.use(router)
+app.mount('#app')
